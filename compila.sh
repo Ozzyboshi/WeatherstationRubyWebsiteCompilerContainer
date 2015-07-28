fc2685f642bafab91a8bbc06efde55a5dd597ece
Alessio Garzi <gun101@email.it>
2015-07-28 20:00:22 +0200

    Update compila.sh

--- compila.sh
+++ compila.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/Ozzyboshi/WeatherstationJavaWS.git /tmp/WeatherStation
 cp -r /tmp/WeatherStation/build/classes /tmp/WeatherStation/WebContent/WEB-INF/
-cd /tmp/WeatherStation/WebContent && jar -cvf $CATALINA_HOME/webapps/WeatherStation.war *
+cd /tmp/WeatherStation/WebContent && jar -cvf /usr/local/tomcat/webapps/WeatherStation.war *
